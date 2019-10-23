ca480b3eaa4a8675239dc8d4bd08e1add47e2a4f
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2019-10-24 00:00:03 +0400

    flag extension

--- flag.py
+++ flag.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 
-size = width, height = 300, 300
+size = width, height = 300, 500
 screen = pygame.display.set_mode(size)
 
 
@@ -11,7 +11,7 @@ def draw_flag():
     white = pygame.Color("#ffffff")
     blue = pygame.Color("#0000ff")
     red = pygame.Color("#ff0000")
-    pygame.draw.rect(screen, gray, [(20, 20), (5, 280)], 0)
+    pygame.draw.rect(screen, gray, [(20, 20), (5, 480)], 0)
     pygame.draw.rect(screen, white, [(25, 20), (200, 80)], 0)
     pygame.draw.rect(screen, blue, [(25, 100), (200, 80)], 0)
     pygame.draw.rect(screen, red, [(25, 180), (200, 80)], 0)
