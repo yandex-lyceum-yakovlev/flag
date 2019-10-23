2f64899cc6993a4f2b6492f96c4cf481fbd5b5fe
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2019-10-23 23:38:32 +0400

    white

--- flag.py
+++ flag.py
@@ -8,7 +8,9 @@ screen = pygame.display.set_mode(size)
 
 def draw_flag():
     gray = pygame.Color("#777777")
+    white = pygame.Color("#ffffff")
     pygame.draw.rect(screen, gray, [(20, 20), (5, 280)], 0)
+    pygame.draw.rect(screen, white, [(25, 20), (200, 80)], 0)
 
 
 draw_flag()
