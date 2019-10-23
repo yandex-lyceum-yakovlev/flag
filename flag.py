2af162c1750029694e52ad6fe0aca2cf5b5fc8cd
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2019-10-23 23:54:12 +0400

    red

--- flag.py
+++ flag.py
@@ -10,9 +10,11 @@ def draw_flag():
     gray = pygame.Color("#777777")
     white = pygame.Color("#ffffff")
     blue = pygame.Color("#0000ff")
+    red = pygame.Color("#ff0000")
     pygame.draw.rect(screen, gray, [(20, 20), (5, 280)], 0)
     pygame.draw.rect(screen, white, [(25, 20), (200, 80)], 0)
     pygame.draw.rect(screen, blue, [(25, 100), (200, 80)], 0)
+    pygame.draw.rect(screen, red, [(25, 180), (200, 80)], 0)
 
 
 draw_flag()
